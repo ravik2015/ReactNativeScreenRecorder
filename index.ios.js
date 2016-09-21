84e931949e2a1845375e0725e1a4e3b0f996dc4c
Vince Yuan <vince.yuan@gmail.com>
2016-09-21 23:44:24 +0800

    Add a list view with random background color rows.

--- index.ios.js
+++ index.ios.js
@@ -7,18 +7,49 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
 class ReactRecorder extends Component {
+  getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  constructor(props) {
+    super(props);
+    var count = 30;
+    var rowsData = [];
+    for (var i = 0; i < count; i++) {
+      rowsData.push({index: i, color: this.getRandomColor()});
+    }
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.index !== r2.index});
+    this.state = {
+      dataSource: ds.cloneWithRows(rowsData)
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.title}>
+          ReactRecorder
         </Text>
+        <ListView style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={
+            (rowData) =>
+            <Text style={{backgroundColor: rowData.color,
+              padding: styles.listViewRowPadding}}>
+              {"Hello, this is Row " + (rowData.index + 1) + "."}
+            </Text>
+          }
+        />
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
@@ -38,11 +69,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    marginTop: 20,
+    marginBottom: 5,
   },
+  listView: {
+    alignSelf: 'stretch',
+  },
+  listViewRowPadding: 10,
+  listViewRowFontSize: 20,
   instructions: {
     textAlign: 'center',
     color: '#333333',
